0ab85d70441b987328ba380122a833c6da1c8e13
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-05-26 12:06:00 +0300

    Folder relocations + devtools demo addded

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/example/demo/rest/FunRestController.java
@@ -0,0 +1,22 @@
+package com.example.demo.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${author.name}")
+    private String authorName;
+    // expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+    // expose "/author" that return the name of author located in src.main.resources.application.properties
+    @GetMapping("/author")
+    public String getAuthorName() {
+        return authorName;
+    }
+}
---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/example/demo/rest/FunRestController.java
@@ -0,0 +1,36 @@
+package com.example.demo.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${author.name}")
+    private String authorName;
+
+    // expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    // expose "/author" that return the name of author located in src.main.resources.application.properties
+    @GetMapping("/author")
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    // expose "/workout" that return a daily workout
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    // expose "/fortune" that tell you the future :O
+    @GetMapping("/fortune")
+    public String getFortune(){
+        return "Your lucky day!";
+    }
+}
